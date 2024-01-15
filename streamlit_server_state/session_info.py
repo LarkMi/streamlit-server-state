9dde04da914f0eef595e6d452ba89d414afd9822
LarkMi <51182700+LarkMi@users.noreply.github.com>
2024-01-15 13:12:08 +0800

    Update session_info.py

--- streamlit_server_state/session_info.py
+++ streamlit_server_state/session_info.py
@@ -57,11 +57,11 @@ def get_this_session() -> AppSession:
     print('this_session_info:',this_session_info)
     while this_session_info is None:
         this_session_info = get_this_session_info()
-            '''
-            if this_session_info is None:
-                raise NoSessionError(
-                   "Oh noes. Couldn't get your Streamlit Session object. "
-                    "Are you doing something fancy with threads?"
-                )
-            '''
+        '''
+        if this_session_info is None:
+            raise NoSessionError(
+               "Oh noes. Couldn't get your Streamlit Session object. "
+                "Are you doing something fancy with threads?"
+            )
+        '''
     return this_session_info.session
