8a342f6d8948f8a43c34045e1c95c4b64db56187
LarkMi <51182700+LarkMi@users.noreply.github.com>
2024-01-15 16:10:50 +0800

    Update session_info.py

--- streamlit_server_state/session_info.py
+++ streamlit_server_state/session_info.py
@@ -55,7 +55,7 @@ def get_this_session_info() -> Optional[SessionInfo]:
 def get_this_session() -> AppSession:
     this_session_info = get_this_session_info()
     print('this_session_info:',this_session_info)
-    while this_session_info is None:
+    if this_session_info is None:
         this_session_info = get_this_session_info()
         '''
         if this_session_info is None:
